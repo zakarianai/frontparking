a3e0603439b15972ec74e3e68c1efd6069eca720
zakarianai <zakarianaim56@gmail.com>
2024-05-09 00:30:40 +0100

    v1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import {useAppContext} from './AppContext';
+import Loader from './components/Layout/Loader';
+
+const AuthPage = React.lazy(() => import('./pages/AuthPage'));
+const HomePage = React.lazy(() => import('./pages/HomePage'));
+const ProfilePage = React.lazy(() => import('./pages/ProfilePage'));
+const BookingsPage = React.lazy(() => import('./pages/BookingsPage'));
+const Presence = React.lazy(() => import('./components/StartingPage/Presence'));
 
 function App() {
+  const {isLoggedIn} = useAppContext();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback = {<Loader />}>
+      <Switch>
+        <Route path='/' exact>
+          <HomePage />
+        </Route>
+
+        <Route path='/auth'>
+          {isLoggedIn && <HomePage />}
+          {!isLoggedIn && <AuthPage />}
+        </Route>
+
+        {isLoggedIn && (
+          <Route path='/profile' exact>
+            <Presence />
+          </Route>
+        )}
+
+        {isLoggedIn && (
+          <Route path='/bookings' exact>
+            <BookingsPage />
+          </Route>
+        )}
+
+        {isLoggedIn && (
+          <Route path = '/profile/:placeId'>
+            <ProfilePage />
+          </Route>
+        )}
+
+        <Route path = '*'>
+          <Redirect to = '/' />
+        </Route>
+      </Switch>
+    </Suspense>
   );
 }
 
